478164eb7b32e41a11c8490eefd1cce90a303b00
Tayyab Chattah <111242774+TayyabChattah@users.noreply.github.com>
2025-02-16 00:51:00 +0500

    img gen with captions

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
 import streamlit as st
-import gradio_client as gc
+from huggingface_hub import InferenceClient
 
-st.title("Ai image genrator")
+client = InferenceClient(model="stabilityai/stable-diffusion-2-1")
+cap_client = InferenceClient(model="gpt2")
 
+st.title("Image generation with caption")
 
+st.sidebar.title("Social Media")
 
+if "selected_platform" not in st.session_state:
+    st.session_state.selected_platform = None
 
-client=gc.Client("black-forest-labs/FLUX.1-dev")
+if st.sidebar.button("Facebook"):
+    st.session_state.selected_platform = "Facebook"
 
-prompt=st.sidebar.text_area("Enter your prompt here: ","")
+if st.session_state.selected_platform == "Facebook":
+    base_prompt = "Generate a powerful and inspiring Facebook post about "
+    user_prompt = st.text_area("Describe additional details:", "")
 
-GenrateImage=st.sidebar.button("Genrate Image")
+    cap_prompt = "the length of prompt should not be greater than 100 words. The post is about "
 
-if GenrateImage:
-    if prompt:
-        result=client.predict(
-            prompt=prompt,
-            seed=0,
-            randomize_seed=True,
-            width=512,
-            height=512,
-            num_inference_steps=4,
-            api_name="/infer"
+    full_prompt = base_prompt + user_prompt
+    full_cap_prompt = cap_prompt + user_prompt
 
-        )
-
-
-
+    if st.button("Generate Image"):
+        if user_prompt:
+            with st.spinner("Generating image..."):
+                try:
+                    image = client.text_to_image(full_prompt, guidance_scale=7.5)
+                    
+                    st.image(image, caption="Generated Image")
+                    
+                    with st.spinner("Generating caption"):
+                        try:
+                            caption = cap_client.text_generation(full_cap_prompt,max_new_tokens=100,temperature=0.7)
+                            st.subheader("Generated Caption")
+                            st.write(caption)
+                        except Exception as e:
+                            st.error(f"An error occurred while generating caption: {e}")
+                except Exception as e:
+                    st.error(f"An error occurred while generating image: {e}")
+        else:
+            st.warning("Please enter a prompt.")
